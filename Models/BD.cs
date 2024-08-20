aab146dc7eb00ff2d22163c89a13b796ce4584ad
FACUNDO IVAN LEWKOWICZ <48178306@ort>
2024-08-20 08:21:46 -0300

    Creacion Proyecto

---
+++ Models/BD.cs
@@ -0,0 +1,9 @@
+using System.Data.SqlClient;
+using Dapper;
+namespace JJOO.Models;
+
+
+public static class BD
+{
+
+}
---
+++ Models/deportista.cs
@@ -0,0 +1,16 @@
+namespace JJOO.Models;
+
+public class Deporte
+{
+   
+    public string Nombre { get; set; }
+
+    public int AñoInicio{ get; set; }
+    public string Sinopsis { get; set; }
+    public string ImagenSerie { get; set; }
+
+    public Deporte()
+    {
+        
+    }
+}
